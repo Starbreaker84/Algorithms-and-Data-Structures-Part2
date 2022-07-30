f922c00fb6ec90cbc3b84170a06bc74b58b55ae5
Ruslan Omalaev <89082306+Starbreaker84@users.noreply.github.com>
2022-07-30 15:37:22 +0300

    Add files via upload

---
+++ SimpleTree/SimpleTreeNode.java
@@ -0,0 +1,112 @@
+import java.util.*;
+
+public class SimpleTreeNode<T>
+{
+    public int NodeLevel;
+    public T NodeValue;
+    public SimpleTreeNode<T> Parent;
+    public List<SimpleTreeNode<T>> Children;
+
+    public SimpleTreeNode(T val, SimpleTreeNode<T> parent)
+    {
+        NodeValue = val;
+        Parent = parent;
+        Children = null;
+    }
+}
+
+class SimpleTree<T>
+{
+    public SimpleTreeNode<T> Root;
+
+    public SimpleTree(SimpleTreeNode<T> root)
+    {
+        Root = root;
+        Root.NodeLevel = 0;
+    }
+    private void AddLevels(SimpleTreeNode<T> root){
+        if (root.Children == null || root.Children.isEmpty()) return;
+        for (SimpleTreeNode<T> node : root.Children){
+            node.NodeLevel = root.NodeLevel + 1;
+            AddLevels(node);
+        }
+    }
+
+    public void AddNodesLevels(){
+        if (Root != null) {
+            Root.NodeLevel = 0;
+            AddLevels(Root);
+        }
+    }
+
+    public void AddChild(SimpleTreeNode<T> ParentNode, SimpleTreeNode<T> NewChild)
+    {
+        if (ParentNode.Children == null) {
+            ParentNode.Children = new ArrayList<>();
+            }
+        ParentNode.Children.add(NewChild);
+        NewChild.Parent = ParentNode;
+        NewChild.NodeLevel = NewChild.Parent.NodeLevel + 1;
+    }
+
+    public void DeleteNode(SimpleTreeNode<T> NodeToDelete)
+    {
+        if (NodeToDelete.Parent != null) {
+            NodeToDelete.Parent.Children.remove(NodeToDelete);
+        }
+    }
+
+    private List<SimpleTreeNode<T>> getNodes(SimpleTreeNode<T> rootNode) {
+        List<SimpleTreeNode<T>> nodes = new ArrayList<>();
+        nodes.add(rootNode);
+        if (rootNode.Children == null || rootNode.Children.isEmpty()) {
+            return nodes;
+        }
+        for (SimpleTreeNode<T> node : rootNode.Children){
+            nodes.addAll(getNodes(node));
+        }
+        return nodes;
+    }
+
+    public List<SimpleTreeNode<T>> GetAllNodes()
+    {
+        if (Root == null) return Collections.emptyList();
+        return getNodes(Root);
+    }
+
+    public List<SimpleTreeNode<T>> FindNodesByValue(T val)
+    {
+        List<SimpleTreeNode<T>> nodes = new ArrayList<>();
+        for (SimpleTreeNode<T> node : GetAllNodes()){
+            if (node.NodeValue == val) nodes.add(node);
+        }
+        return nodes;
+    }
+
+    public void MoveNode(SimpleTreeNode<T> OriginalNode, SimpleTreeNode<T> NewParent)
+    {
+        if (OriginalNode.Parent != null) {
+            DeleteNode(OriginalNode);
+            AddChild(NewParent, OriginalNode);
+        }
+    }
+
+    public int Count()
+    {
+        return GetAllNodes().size();
+    }
+
+    private int getLeafs(SimpleTreeNode<T> root){
+        if (root.Children == null || root.Children.isEmpty()) return 1;
+        int leafs = 0;
+        for (SimpleTreeNode<T> node : root.Children){
+            leafs += getLeafs(node);
+        }
+        return leafs;
+    }
+
+    public int LeafCount(){
+        if (Root == null) return 0;
+        return getLeafs(Root);
+    }
+}
---
+++ SimpleTree/SimpleTreeNodeTest.java
@@ -0,0 +1,211 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleTreeNodeTest {
+
+    @Test
+    void AddChild_test(){
+        SimpleTreeNode<Integer> node1 = new SimpleTreeNode<>(9, null);
+        SimpleTreeNode<Integer> node2 = new SimpleTreeNode<>(4, null);
+        SimpleTreeNode<Integer> node4 = new SimpleTreeNode<>(3, null);
+
+        SimpleTree<Integer> tree = new SimpleTree<>(node1);
+
+        tree.AddChild(node1, node2);
+        assertEquals(node2, node1.Children.get(0));
+        tree.AddChild(node2, node4);
+        assertEquals(node4, node2.Children.get(0));
+    }
+
+    @Test
+    void DeleteNode_test(){
+        SimpleTreeNode<Integer> node1 = new SimpleTreeNode<>(9, null);
+        SimpleTreeNode<Integer> node2 = new SimpleTreeNode<>(4, null);
+        SimpleTreeNode<Integer> node4 = new SimpleTreeNode<>(3, null);
+        SimpleTreeNode<Integer> node5 = new SimpleTreeNode<>(6, null);
+        SimpleTreeNode<Integer> node7 = new SimpleTreeNode<>(5, null);
+        SimpleTreeNode<Integer> node8 = new SimpleTreeNode<>(7, null);
+
+        SimpleTree<Integer> tree = new SimpleTree<>(node1);
+
+        tree.AddChild(node1, node2);
+        tree.AddChild(node2, node4);
+        tree.AddChild(node2, node5);
+        tree.AddChild(node5, node7);
+        tree.AddChild(node5, node8);
+
+        tree.DeleteNode(node5);
+        assertEquals(1, node2.Children.size());
+        assertEquals(node4, node2.Children.get(0));
+        tree.DeleteNode(node1);
+        assertEquals(node1, tree.Root);
+    }
+
+    @Test
+    void GetAllNodes_test(){
+        SimpleTreeNode<Integer> node1 = new SimpleTreeNode<>(9, null);
+        SimpleTreeNode<Integer> node2 = new SimpleTreeNode<>(4, null);
+        SimpleTreeNode<Integer> node3 = new SimpleTreeNode<>(17, null);
+        SimpleTreeNode<Integer> node4 = new SimpleTreeNode<>(3, null);
+        SimpleTreeNode<Integer> node5 = new SimpleTreeNode<>(6, null);
+        SimpleTreeNode<Integer> node6 = new SimpleTreeNode<>(22, null);
+        SimpleTreeNode<Integer> node7 = new SimpleTreeNode<>(5, null);
+        SimpleTreeNode<Integer> node8 = new SimpleTreeNode<>(7, null);
+        SimpleTreeNode<Integer> node9 = new SimpleTreeNode<>(20, null);
+
+        SimpleTree<Integer> tree = new SimpleTree<>(node1);
+
+        tree.AddChild(node1, node2);
+        tree.AddChild(node2, node4);
+        tree.AddChild(node2, node5);
+        tree.AddChild(node5, node7);
+        tree.AddChild(node5, node8);
+        tree.AddChild(node1, node3);
+        tree.AddChild(node3, node6);
+        tree.AddChild(node6, node9);
+
+        List<SimpleTreeNode<Integer>> comparableList = new ArrayList<>();
+        comparableList.add(node1);
+        comparableList.add(node2);
+        comparableList.add(node4);
+        comparableList.add(node5);
+        comparableList.add(node7);
+        comparableList.add(node8);
+        comparableList.add(node3);
+        comparableList.add(node6);
+        comparableList.add(node9);
+
+        assertEquals(comparableList, tree.GetAllNodes());
+    }
+
+    @Test
+    void FindNodesByValue_test(){
+        SimpleTreeNode<Integer> node1 = new SimpleTreeNode<>(9, null);
+        SimpleTreeNode<Integer> node2 = new SimpleTreeNode<>(4, null);
+        SimpleTreeNode<Integer> node3 = new SimpleTreeNode<>(17, null);
+        SimpleTreeNode<Integer> node4 = new SimpleTreeNode<>(3, null);
+        SimpleTreeNode<Integer> node5 = new SimpleTreeNode<>(6, null);
+        SimpleTreeNode<Integer> node6 = new SimpleTreeNode<>(6, null);
+        SimpleTreeNode<Integer> node7 = new SimpleTreeNode<>(5, null);
+        SimpleTreeNode<Integer> node8 = new SimpleTreeNode<>(6, null);
+        SimpleTreeNode<Integer> node9 = new SimpleTreeNode<>(20, null);
+
+        SimpleTree<Integer> tree = new SimpleTree<>(node1);
+
+        tree.AddChild(node1, node2);
+        tree.AddChild(node2, node4);
+        tree.AddChild(node2, node5);
+        tree.AddChild(node5, node7);
+        tree.AddChild(node5, node8);
+        tree.AddChild(node1, node3);
+        tree.AddChild(node3, node6);
+        tree.AddChild(node6, node9);
+
+        List<SimpleTreeNode<Integer>> comparableList = new ArrayList<>();
+        comparableList.add(node5);
+        comparableList.add(node8);
+        comparableList.add(node6);
+
+        assertEquals(comparableList, tree.FindNodesByValue(6));
+    }
+
+    @Test
+    void MoveNode_test(){
+        SimpleTreeNode<Integer> node1 = new SimpleTreeNode<>(9, null);
+        SimpleTreeNode<Integer> node2 = new SimpleTreeNode<>(4, null);
+        SimpleTreeNode<Integer> node3 = new SimpleTreeNode<>(17, null);
+        SimpleTreeNode<Integer> node4 = new SimpleTreeNode<>(3, null);
+        SimpleTreeNode<Integer> node5 = new SimpleTreeNode<>(6, null);
+        SimpleTreeNode<Integer> node6 = new SimpleTreeNode<>(22, null);
+        SimpleTreeNode<Integer> node7 = new SimpleTreeNode<>(5, null);
+        SimpleTreeNode<Integer> node8 = new SimpleTreeNode<>(7, null);
+        SimpleTreeNode<Integer> node9 = new SimpleTreeNode<>(20, null);
+
+        SimpleTree<Integer> tree = new SimpleTree<>(node1);
+
+        tree.AddChild(node1, node2);
+        tree.AddChild(node2, node4);
+        tree.AddChild(node2, node5);
+        tree.AddChild(node5, node7);
+        tree.AddChild(node5, node8);
+        tree.AddChild(node1, node3);
+        tree.AddChild(node3, node6);
+        tree.AddChild(node6, node9);
+
+        tree.MoveNode(node5, node6);
+
+        assertEquals(1, node2.Children.size());
+        assertEquals(node4, node2.Children.get(0));
+        assertEquals(2, node6.Children.size());
+        assertEquals(node9, node6.Children.get(0));
+        assertEquals(node5, node6.Children.get(1));
+        assertEquals(2, node5.Children.size());
+        assertEquals(node7, node5.Children.get(0));
+        assertEquals(node8, node5.Children.get(1));
+    }
+
+    @Test
+    void Count_test(){
+        SimpleTreeNode<Integer> node1 = new SimpleTreeNode<>(9, null);
+        SimpleTreeNode<Integer> node2 = new SimpleTreeNode<>(4, null);
+        SimpleTreeNode<Integer> node3 = new SimpleTreeNode<>(17, null);
+        SimpleTreeNode<Integer> node4 = new SimpleTreeNode<>(3, null);
+        SimpleTreeNode<Integer> node5 = new SimpleTreeNode<>(6, null);
+        SimpleTreeNode<Integer> node6 = new SimpleTreeNode<>(22, null);
+        SimpleTreeNode<Integer> node7 = new SimpleTreeNode<>(5, null);
+        SimpleTreeNode<Integer> node8 = new SimpleTreeNode<>(7, null);
+        SimpleTreeNode<Integer> node9 = new SimpleTreeNode<>(20, null);
+
+        SimpleTree<Integer> tree = new SimpleTree<>(node1);
+
+        tree.AddChild(node1, node2);
+        tree.AddChild(node2, node4);
+        tree.AddChild(node2, node5);
+        tree.AddChild(node5, node7);
+        tree.AddChild(node5, node8);
+        tree.AddChild(node1, node3);
+        tree.AddChild(node3, node6);
+        tree.AddChild(node6, node9);
+
+        assertEquals(9, tree.Count());
+
+        tree.DeleteNode(node5);
+        assertEquals(6, tree.Count());
+    }
+
+    @Test
+    void LeafCount_test(){
+        SimpleTreeNode<Integer> node1 = new SimpleTreeNode<>(9, null);
+        SimpleTreeNode<Integer> node2 = new SimpleTreeNode<>(4, null);
+        SimpleTreeNode<Integer> node3 = new SimpleTreeNode<>(17, null);
+        SimpleTreeNode<Integer> node4 = new SimpleTreeNode<>(3, null);
+        SimpleTreeNode<Integer> node5 = new SimpleTreeNode<>(6, null);
+        SimpleTreeNode<Integer> node6 = new SimpleTreeNode<>(22, null);
+        SimpleTreeNode<Integer> node7 = new SimpleTreeNode<>(5, null);
+        SimpleTreeNode<Integer> node8 = new SimpleTreeNode<>(7, null);
+        SimpleTreeNode<Integer> node9 = new SimpleTreeNode<>(20, null);
+
+        SimpleTree<Integer> tree = new SimpleTree<>(node1);
+
+        tree.AddChild(node1, node2);
+        tree.AddChild(node2, node4);
+        tree.AddChild(node2, node5);
+        tree.AddChild(node5, node7);
+        tree.AddChild(node5, node8);
+        tree.AddChild(node1, node3);
+        tree.AddChild(node3, node6);
+        tree.AddChild(node6, node9);
+
+        assertEquals(4, tree.LeafCount());
+
+        tree.DeleteNode(node5);
+        assertEquals(2, tree.LeafCount());
+
+        tree.DeleteNode(node2);
+        assertEquals(1, tree.LeafCount());
+    }
+}
